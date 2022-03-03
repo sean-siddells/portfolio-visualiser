6220498de47353e35c1312b4c0e809b5e0fd92b6
sean <seansiddells@gmail.com>
2022-03-03 19:49:14 +1300

    verrry basic set up and hosting added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # trail-dvd-visualiser
 Toy Project for creating a basic visualiser in p5.js
+
+--------
+
+run the following command:
+
+```npm install -g http-server```
+
+then navigate into the public directory and run:
+
+```http-server ./```
---
+++ public/sketch.js
@@ -0,0 +1,48 @@
+let x
+let y
+
+let xspeed
+let yspeed
+
+let r, g, b
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  x= random(width - 80)
+  y= random(height - 60)
+  xspeed= 3
+  yspeed= 3
+  pickColour()
+}
+
+function pickColour() {
+  r = random(100, 256)
+  g = random(100, 256)
+  b = random(100, 256)
+}
+
+function draw() {
+  rect(x, y, windowWidth/20, windowWidth/35, 15)
+  let c = color(r, g, b)
+  fill(c)
+  x = x + xspeed
+  y = y + yspeed
+
+  if (x + windowWidth/20 >= width) {
+    xspeed = -xspeed
+    pickColour()
+  } else if (x <= 0) {
+    xspeed = -xspeed
+    x= 0
+    pickColour()
+  }
+
+  if (y + windowWidth/35 >= height) {
+    yspeed = -yspeed
+    pickColour()
+  } else if (y <= 0) {
+    yspeed= -yspeed
+    y= 0
+    pickColour()
+  }
+}
